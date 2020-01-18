93b59eb3ee2c19e615a769d4bfb7b3f6a46a56a4
TylerMah <ttmah@ualberta.ca>
2020-01-18 15:57:40 -0700

    added basic statements to sort jobs

---
+++ dataSort.py
@@ -0,0 +1,22 @@
+possibleJobs = []
+declineJobs = []
+coverLetterJobs = []
+
+for key in jobs:
+    if 1 in jobs[key][1]:
+        possibleJobs.append(key)
+    else:
+        declineJobs.append(key)
+
+for key in possibleJobs:
+    if 4 not in jobs[key][2]:
+        i = possibleJobs.index(key)
+        del possibleJobs[i]
+        declineJobs.append[key]
+
+for key in possibleJobs:
+    if coverLetter in jobs[key][4]:
+        i = possibleJobs.index(k)
+        del possibleJobs[i]
+        coverLetterJobs.append(key)
+
