2d17bd5375702649656d8fe00c6c62f4941a5329
Steven Jiao <stevenjiao@MacBook-Pro.local>
2020-01-18 17:31:31 -0700

    webscraper testing

--- webScraper.py
+++ webScraper.py
@@ -1,16 +1,31 @@
 import requests
+from selenium import webdriver
 from lxml import html
 from bs4 import BeautifulSoup
+127.0.0.1   localhost
 
-page = requests.get("https://www.placeprocanada.com/students/JobDetails.aspx?ID=55735&TID=3&YID=2020&CID=5098")
-soup = BeautifulSoup(page.content, 'html.parser')
+def scraper():
+    driver = webdriver.Chrome("/Users/stevenjiao/Documents/chromedriver")
+    driver.get("https://www.placeprocanada.com/students/default.aspx")
+    print(driver.title)
+    driver.close()
+    
+    #driver.find_element_by_xpath("""//*[@id="txtLogin"]""").send_keys("StevenJiao")
+    #driver.find_element_by_xpath("""//*[@id="txtPassword"]""").send_keys("StevenJiao1")
+    #driver.find_element_by_xpath("""//*[@id="txtAccessCode"]""").send_keys("uofastu")
+    #driver.find_element_by_xpath("""//*[@id="btnLogin"]""").click()
+    #driver.get("https://www.placeprocanada.com/students/JobDetails.aspx?ID=55735&TID=3&YID=2020&CID=5098")
 
-print(soup)
+    #soup = BeautifulSoup(driver.page_source, 'html.parser')
+    #print(soup)
 
 def login():
-    session_requests = requests.session()
-    login_url = "https://www.placeprocanada.com/students/JobDetails.aspx?ID=55735&TID=3&YID=2020&CID=5098"
-    login_result = session_requests.get(login_url)
+    
+    scraper()
+
+
+if __name__ == '__main__':
+    scraper()
 
 # Key: Company Name
 # Array index order: Job title, Work level, Work duration, Location, Cover letter requirement
