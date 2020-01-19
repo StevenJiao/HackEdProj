800e10bc3308dd7f9aa6064891c32bae08bb5238
Steven Jiao <stevenjiao@MacBook-Pro.local>
2020-01-18 18:02:28 -0700

    new webscraper

--- webScraper.py
+++ webScraper.py
@@ -4,23 +4,10 @@ from lxml import html
 from bs4 import BeautifulSoup
 
 def scraper():
-    driver = webdriver.Chrome("/Users/stevenjiao/Documents/chromedriver")
-    driver.get("https://www.placeprocanada.com/students/default.aspx")
-    print(driver.title)
-    driver.close()
-    
-    #driver.find_element_by_xpath("""//*[@id="txtLogin"]""").send_keys("StevenJiao")
-    #driver.find_element_by_xpath("""//*[@id="txtPassword"]""").send_keys("StevenJiao1")
-    #driver.find_element_by_xpath("""//*[@id="txtAccessCode"]""").send_keys("uofastu")
-    #driver.find_element_by_xpath("""//*[@id="btnLogin"]""").click()
-    #driver.get("https://www.placeprocanada.com/students/JobDetails.aspx?ID=55735&TID=3&YID=2020&CID=5098")
+    page = requests.get("https://www.ualberta.ca/kinesiology-sport-recreation/campus-community-recreation/drop-in/badminton")
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print(soup.prettify())
 
-    #soup = BeautifulSoup(driver.page_source, 'html.parser')
-    #print(soup)
-
-def login():
-    
-    scraper()
 
 
 if __name__ == '__main__':
